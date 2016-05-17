11a70d9ce361df246a35017d8259aec24219e332
MaxwellTang <tjx19920310@163.com>
2016-05-18 00:30:58 +0800

    add

---
+++ toposcript/mytopo.py
@@ -0,0 +1,52 @@
+from mininet.topo import Topo
+
+class mytopo( Topo ):
+
+    def __init__(self):
+
+        #Init topo
+        Topo.__init__(self)
+        L1=2
+        L2=L1*2
+        L3=L2
+        c=[]
+        a=[]
+        e=[]
+
+        #add core ovs
+        for i in range(L1):
+            sw =self.addSwitch('c{}'.format(i+1))
+            c.append(sw)
+
+        #add aggregation ovs
+        for i in range(L2):
+            sw =self.addSwitch('a{}'.format(L1+i+1))
+            a.append(sw)
+
+        #add edge ovs
+        for i in range(L3):
+            sw =self.addSwitch('e{}'.format(L1+L2+i+1))
+            e.append(sw)
+
+        #add links between core and aggregation ovs
+        for i in range(L1):
+            sw1 = c[i]
+            for sw2 in a[i/2::L1/2]:
+                self.addLink(sw2,sw1)
+
+        #add links between aggregation and edge ovs
+        for i in range(0,L2,2):
+            for sw1 in a[i:i+2]:
+                for sw2 in e[i:i+2]:
+                    self.addLink(sw2,sw1)
+
+        #add links between hosts and edge ovs
+        count = 1
+        for sw1 in e:
+            for i in range(2):
+                host=self.addHost('h{}'.format(count))
+                self.addLink(sw1,host)
+                count = count + 1
+
+topos={'mytopo':(lambda:mytopo())}
+
